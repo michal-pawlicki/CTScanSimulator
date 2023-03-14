9132a69ac1b795c3c0f9f73e806a9d26ecf47097
michal-pawlicki <michalpawlicki02@gmail.com>
2023-03-14 20:09:37 +0100

    added file selection

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+import os
+
 import streamlit as st
+import os
+from PIL import Image
 
 
 def main():
     st.title('CT scan simulator')
     st.subheader('by Agnieszka Grzymaska and Michał Pawlicki')
     st.markdown('---')
-    side_bar()
+    file = side_bar()
+    image = Image.open('./images/'+file)
+    st.image(image, width=300)
+    st.text(image.getpixel((1,2)))
 
 
 def side_bar():
@@ -16,6 +23,12 @@ def side_bar():
     st.sidebar.select_slider("Select value of n", options=range(1, 20, 2))
     st.sidebar.markdown("""$Span$ $of$ $the$ $emitter$ $system$""")
     st.sidebar.select_slider("Select value of l", options=range(1, 20, 2))
+    files = os.listdir('./images')
+    st.sidebar.markdown("""$File$ $to$ $scan$""")
+    file = st.sidebar.selectbox(
+        'Choose a file to read',
+        files)
+    return file
 
 
 if __name__ == '__main__':

fd21bfc98bb9cc2f324df7c758f7b2da32e6d65f
michal-pawlicki <michalpawlicki02@gmail.com>
2023-03-27 20:53:35 +0200

    added radon transform

--- main.py
+++ main.py
@@ -1,8 +1,45 @@
-import os
-
+import numpy as np
 import streamlit as st
 import os
-from PIL import Image
+import matplotlib.pyplot as plt
+import math
+from bresenham import bresenham
+
+
+def radon_transform(image, num_angles, num_detect, theta):
+    global min
+    shape_min = min(image.shape[:2])
+    r = shape_min // 2 - 1
+
+    angles = np.linspace(0, np.pi*2, num_angles, endpoint=False)
+    sinogram = np.zeros((num_angles, num_detect, 3))
+
+    for i, angle in enumerate(angles):
+        x1 = int(r + r * math.cos(angle))
+        y1 = int(r + r * math.sin(angle))
+        for j in range(num_detect):
+            x2 = int(r + r * math.cos(angle + np.pi - theta/2 + j * theta/(num_detect-1)))
+            y2 = int(r + r * math.sin(angle + np.pi - theta/2 + j * theta/(num_detect-1)))
+            line = list(bresenham(x1, y1, x2, y2))
+            value = 0
+
+            print(angle, r, x1,y1,x2,y2)
+            for element in line:
+                if len(image.shape) == 3:
+                    value += image[element[0], element[1], 0]
+                if len(image.shape) == 2:
+                    value += image[element[0], element[1]]
+            sinogram[i, j, 0] += value
+    max = sinogram.max()
+    min = sinogram.min()
+    print(min, max)
+    for x in range(len(sinogram)):
+        for y in range(len(sinogram[0])):
+            value = (sinogram[x, y, 0]-min)/(max-min)
+            sinogram[x, y, 0] = value
+            sinogram[x, y, 1] = value
+            sinogram[x, y, 2] = value
+    return sinogram
 
 
 def main():
@@ -10,9 +47,10 @@ def main():
     st.subheader('by Agnieszka Grzymaska and Michał Pawlicki')
     st.markdown('---')
     file = side_bar()
-    image = Image.open('./images/'+file)
+    image = plt.imread('./images/'+file, format='gray')
+    sinogram = radon_transform(image, 100, 100, np.pi/2)
     st.image(image, width=300)
-    st.text(image.getpixel((1,2)))
+    st.image(sinogram, width=300)
 
 
 def side_bar():

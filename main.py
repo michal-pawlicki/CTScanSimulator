cbdb3493057daea9c9406eddbb27a5b851718cdb
Agnieszka <agnieszkagrzymska1412@gmail.com>
2023-03-28 21:09:26 +0200

    User input
    Added user input of alpha angle, number of detectors and the span angle

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def radon_transform(image, num_angles, num_detect, theta):
             line = list(bresenham(x1, y1, x2, y2))
             value = 0
 
-            print(angle, r, x1,y1,x2,y2)
+            #print(angle, r, x1,y1,x2,y2)
             for element in line:
                 if len(image.shape) == 3:
                     value += image[element[0], element[1], 0]
@@ -32,7 +32,7 @@ def radon_transform(image, num_angles, num_detect, theta):
             sinogram[i, j, 0] += value
     max = sinogram.max()
     min = sinogram.min()
-    print(min, max)
+    #print(min, max)
     for x in range(len(sinogram)):
         for y in range(len(sinogram[0])):
             value = (sinogram[x, y, 0]-min)/(max-min)
@@ -46,9 +46,9 @@ def main():
     st.title('CT scan simulator')
     st.subheader('by Agnieszka Grzymska and Michał Pawlicki')
     st.markdown('---')
-    file = side_bar()
+    file, num_angles, num_detect, theta = side_bar()
     image = plt.imread('./images/'+file, format='gray')
-    sinogram = radon_transform(image, 100, 100, np.pi/2)
+    sinogram = radon_transform(image, num_angles, num_detect, theta)
     st.image(image, width=300)
     st.image(sinogram, width=300)
 
@@ -56,17 +56,20 @@ def main():
 def side_bar():
     st.sidebar.markdown('# Set scanner options')
     st.sidebar.markdown(""" $\Delta$ $\\alpha$ $value$""")
-    st.sidebar.select_slider("Select value of alpha", options=range(1, 20, 2))
+    angles = [1,2,3,5,10,15,18,20,30,36,40,45,60,72,90,120,180,360]
+    alpha = st.sidebar.select_slider("Select value of alpha", options=angles)
+    num_angles = int(360/alpha)
     st.sidebar.markdown("""$Number$ $of$ $detectors$""")
-    st.sidebar.select_slider("Select value of n", options=range(1, 20, 2))
+    num_detect = st.sidebar.select_slider("Select value of n", options=range(10, 101, 10))
     st.sidebar.markdown("""$Span$ $of$ $the$ $emitter$ $system$""")
-    st.sidebar.select_slider("Select value of l", options=range(1, 20, 2))
+    span = st.sidebar.select_slider("Select value of l", options=range(1, 361))
+    theta = math.radians(span)
     files = os.listdir('./images')
     st.sidebar.markdown("""$File$ $to$ $scan$""")
     file = st.sidebar.selectbox(
         'Choose a file to read',
         files)
-    return file
+    return file, num_angles, num_detect, theta
 
 
 if __name__ == '__main__':
